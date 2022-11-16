6bf97973c932cb2fc2996366a9de6388e50c5f3a
Mayank Gongal <mayankgongal@Mayank-MBA-M1.local>
2022-11-16 12:22:09 +0530

    mello

---
+++ javabasics.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class javabasics{
+    public static void main(String[] args){
+      int n = 10;
+   for(int line=1; line<= n ; line++){
+      for(int number=1; number<=line ; number++){
+         System.out.print(number);
+      }
+      System.out.println();
+      
+   }
+
+
+   }
+}
+// java javabasics.java
+   
